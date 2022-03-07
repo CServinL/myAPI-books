f17d879380e693b7d1b3632da1072fc0822141ca
Emileneth <emileneth@gmail.com>
2022-03-07 16:52:11 -0600

    test sql

--- src/database_schema.sql
+++ src/database_schema.sql
@@ -1,13 +1,10 @@
-IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'myAPI')
-BEGIN
-    CREATE DATABASE [DataBase]
-END
+CREATE DATABASE IF NOT EXISTS myapi
 
-USE [DataBase]
+USE myapi
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='books' and xtype='U')
 BEGIN
-    CREATE TABLE library.books (
+    CREATE TABLE myapi.books (
         ID INT AUTO_INCREMENT PRIMARY KEY,
         ISBN VARCHAR(10) NOT NULL,
         Title VARCHAR(64) NOT NULL
