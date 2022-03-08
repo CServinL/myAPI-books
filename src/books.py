693e2ff4227e0e36b5aa971bbe0281859a36b12d
Emileneth <emileneth@gmail.com>
2022-03-08 03:44:44 -0600

    using sepparate db

--- src/books.py
+++ src/books.py
@@ -15,7 +15,7 @@ def before_request():
             port       =os.environ['MYSQL_PORT'],
             user       =os.environ['MYSQL_USER'],
             password   =os.environ['MYSQL_PASSWORD'],
-            database   ="library"
+            database   ="myapi-books"
             )
 
 @app.teardown_request
